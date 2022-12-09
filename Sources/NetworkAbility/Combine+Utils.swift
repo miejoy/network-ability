236a5b92c2db7eba4ba4da136ae2fbed019979c3
Raymond <raymond0huang@gmail.com>
2022-12-09 23:58:42 +0800

    完善 Combine 工具方法

--- Sources/NetworkAbility/Combine+Utils.swift
+++ Sources/NetworkAbility/Combine+Utils.swift
@@ -9,11 +9,12 @@
 import Combine
 
 extension Publisher {
-    /// 转化为 Future，暂时内部使用
+    /// 转化为 Future，暂时内部使用，注意：这里如果发布者没有传数据直接结束会存在泄漏
     func asFuture() -> Future<Output, Failure> {
+        // 让 block 持有 cancellable
         var cancellable: AnyCancellable?
         return Future<Output, Failure> { promise in
-            // cancellable is captured to assure the completion of the wrapped future
+            var didReceiveValue = false
             cancellable = self.sink { completion in
                 if case .failure(let error) = completion {
                     promise(.failure(error))
@@ -21,16 +22,23 @@ extension Publisher {
                 cancellable = nil
             } receiveValue: { value in
                 promise(.success(value))
+                didReceiveValue = true
                 cancellable?.cancel()
+            }
+            if didReceiveValue {
+                // 确保同步返回的场景不会出现内存泄漏
+                cancellable?.cancel()
+                cancellable = nil
             }
         }
     }
 }
 
 extension Future {
-    /// Future 添加完成回调
+    /// Future 添加完成回调，注意：这里如果发布者没有传数据直接结束会存在泄漏
     public func completion(with block: @escaping (Result<Output, Error>) -> Void) {
         var cancellable: AnyCancellable?
+        var didReceiveValue = false
         cancellable = self.sink { completion in
             if case .failure(let error) = completion {
                 block(.failure(error))
@@ -38,7 +46,13 @@ extension Future {
             cancellable = nil
         } receiveValue: { data in
             block(.success(data))
+            didReceiveValue = true
             cancellable?.cancel()
+        }
+        if didReceiveValue {
+            // 确保同步返回的场景不会出现内存泄漏
+            cancellable?.cancel()
+            cancellable = nil
         }
     }
 }
